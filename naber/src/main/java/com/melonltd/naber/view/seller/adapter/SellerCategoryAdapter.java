82c2d7f7eb8511e9d2b86595b48b81ced547ece0
Evan.W <evan.wang@yesee.com.tw>
2018-09-13 13:11:57 +0800

    no message

--- naber/src/main/java/com/melonltd/naber/view/seller/adapter/SellerCategoryAdapter.java
+++ naber/src/main/java/com/melonltd/naber/view/seller/adapter/SellerCategoryAdapter.java
@@ -5,7 +5,6 @@ import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,16 +12,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.android.gms.common.util.NumberUtils;
-import com.google.android.gms.flags.impl.DataUtils;
-import com.google.common.base.Strings;
-import com.google.common.primitives.Ints;
 import com.melonltd.naber.R;
 import com.melonltd.naber.model.bean.Model;
 import com.melonltd.naber.view.common.HideKeyboardListener;
 import com.melonltd.naber.view.customize.SwitchButton;
-
-import java.text.NumberFormat;
 
 public class SellerCategoryAdapter extends RecyclerView.Adapter<SellerCategoryAdapter.ViewHolder> {
     private static final String TAG = SellerCategoryAdapter.class.getSimpleName();
@@ -41,7 +34,7 @@ public class SellerCategoryAdapter extends RecyclerView.Adapter<SellerCategoryAd
     // TODO 新增可否編輯排序，用法
     // 開啟編輯 adapter.setSortEdit(true).notifyDataSetChanged();
     // 關閉編輯 adapter.setSortEdit(false).notifyDataSetChanged();
-    public SellerCategoryAdapter setSortEdit(boolean isSortEdit){
+    public SellerCategoryAdapter setSortEdit(boolean isSortEdit) {
         this.IS_SORT_EDIT = isSortEdit;
         return this;
     }
@@ -55,7 +48,7 @@ public class SellerCategoryAdapter extends RecyclerView.Adapter<SellerCategoryAd
     }
 
     @Override
-    public void onBindViewHolder(@NonNull final ViewHolder holder,final int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
         holder.v.setOnClickListener(this.hideKeyboardListener);
         holder.topEdit.setText(Model.SELLER_CATEGORY_LIST.get(position).top + "");
 
@@ -63,58 +56,39 @@ public class SellerCategoryAdapter extends RecyclerView.Adapter<SellerCategoryAd
         holder.topEdit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                Log.i(TAG,v.toString());
+                Log.i(TAG, v.toString());
             }
         });
 
         holder.topEdit.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                Log.i(TAG,s.toString());
+                Log.i(TAG, s.toString());
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                Log.i(TAG,s.toString());
+                Log.i(TAG, s.toString());
             }
 
             @Override
             public void afterTextChanged(Editable s) {
                 Log.i(TAG, s.toString());
 
-                Integer tmp = Ints.tryParse(s.toString());
+                Integer tmp = parseInt(s.toString(), 0);
                 Log.i(TAG, tmp.toString());
 
-
-                if (s.toString().length() >= 2){
-
-                }else if (s.toString().length() >= 1){
-
-                }else {
-
-                }
-
-//                if (Strings.isNullOrEmpty(s.toString())){
-//                    Model.SELLER_CATEGORY_LIST.get(position).top = tmp;
-//                    s.clear();
-//                    s.append("0");
-////                    notifyItemChanged(position);
-//                }else if (s.toString().length() >= 2){
-//                    tmp = Ints.tryParse(s.toString());
-//                    Model.SELLER_CATEGORY_LIST.get(position).top = tmp;
-//                    s.clear();
-////                    s.toString().toCharArray();
-//                    char[] cs = tmp.toString().toCharArray();
-//                    for (int i=0; i<cs.length ; i++){
-//                        s.append(cs[i]);
-//                    }
-////                    notifyItemChanged(position);
+                Log.i(TAG, "tmp.length: " + tmp.toString().length() + ", s.length: " + s.toString().length());
+                Model.SELLER_CATEGORY_LIST.get(position).top = tmp;
+//                if (s.toString().length() != tmp.toString().length()) {
+//                    notifyItemChanged(position);
 //                }
 
 
+                Log.i(TAG, "top : " + Model.SELLER_CATEGORY_LIST.get(position).top);
+
             }
         });
-
 
 
         holder.topEdit.setEnabled(this.IS_SORT_EDIT);
@@ -151,10 +125,22 @@ public class SellerCategoryAdapter extends RecyclerView.Adapter<SellerCategoryAd
             this.aSwitch = v.findViewById(R.id.aSwitch);
         }
 
-        public void setTag(int position){
+        public void setTag(int position) {
             this.aSwitch.setTag(position);
             this.editBtn.setTag(position);
             this.deleteBtn.setTag(position);
+        }
+    }
+
+
+    public static int parseInt(String intStr, int dflt) {
+        if (intStr == null)
+            return dflt;
+
+        try {
+            return Integer.parseInt(intStr);
+        } catch (NumberFormatException e) {
+            return dflt;
         }
     }
 
